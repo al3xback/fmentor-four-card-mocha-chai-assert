657cc86e8c63b98f3de00ceb3e4850d017d36500
alex <al3xback@gmail.com>
2024-01-18 10:08:46 +0700

    changed to getAttr func

--- test/main.js
+++ test/main.js
@@ -32,8 +32,8 @@ describe('DOM', () => {
 
 		for (let i = 0; i < cardImgEls.length; i++) {
 			const cardImgEl = cardImgEls[i];
-			const cardImgWidth = cardImgEl.width;
-			const cardImgHeight = cardImgEl.height;
+			const cardImgWidth = parseInt(cardImgEl.getAttribute('width'));
+			const cardImgHeight = parseInt(cardImgEl.getAttribute('height'));
 
 			assert.typeOf(cardImgWidth, 'number');
 			assert.typeOf(cardImgHeight, 'number');
